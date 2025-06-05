e51adc74c6e4d6eb20a59496a9ecfccd5b94af90
IceliosGit <119666701+IceliosGit@users.noreply.github.com>
2025-06-05 23:48:03 +0200

    fixed ytd = youtube_to_dfpwm:main

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'ytd = your_module:main',
+            'ytd = youtube_to_dfpwm:main',
         ],
     },
     python_requires='>=3.7',
